716b532e86153163d8b6cfde8264e1a90e453e9e
Mike San Román <mike@msanroman.io>
2018-04-24 19:06:44 +0200

    Basic html & css

---
+++ form.js
@@ -0,0 +1,7 @@
+/**
+ * 1. Cambiar tabs
+ * 2. Labels se mueven en función de si el elemento está activo (focus) o si no lo está, pero tiene contenido
+ * 3. Validación
+ */
+$(document).ready(function() {
+});
